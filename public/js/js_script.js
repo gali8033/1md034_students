155f2eaa7792d8039363dcf1f8f8d06ada4b454a
Gabriel Lindgren <gabriellindgren1@gmail.com>
2020-01-17 23:12:29 +0100

    first implementation of addEventListener for pure javascript

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -76,3 +76,48 @@ for(let i = 0; i < menu.length; i++) {
 }
 */
 
+let button = document.getElementById("sendDeliveryBtn");
+
+/*
+button.addEventListener("click", function() {
+  console.log("Button clicked!");
+});
+*/
+
+/*
+function getOrder() {
+  let name = document.getElementById("fnln").value;
+  let email = document.getElementById("em").value;
+  let street = document.getElementById("st").value;
+  let houseNumber = document.getElementById("hn").value;
+  let paymentOption = document.getElementById("paymentOption").value;
+  var genders = document.getElementsByName('gender');
+  let checked;
+  for (let i = 0; i < genders.length; i++){
+    if (genders[i].checked){
+      checked = genders[i].value;
+    }
+  }
+
+
+  let returnArray = [name, email, street, houseNumber, paymentOption, checked];
+  return returnArray;
+}
+
+button.addEventListener("click", getOrder);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
