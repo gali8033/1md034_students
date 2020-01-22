ef0e10eed60f8cc6be7897d118ae1ff8036cd3f9
Gabriel Lindgren <gabriellindgren1@gmail.com>
2020-01-22 13:42:31 +0100

    Revert "first commit, added boilerplate code"
    This reverts commit e1cf56f9a85d121f48c17d2ed5081692f5e3312e.

--- app.js
+++ app.js
@@ -15,27 +15,64 @@ app.set('port', (process.env.PORT || port));
 
 // Serve static assets from public/
 app.use(express.static(path.join(__dirname, 'public/')));
+// Serve vue from node_modules as vue/
+
+app.use('/vue', express.static(path.join(__dirname, '/node_modules/vue/dist/')));
 
 // Serve index.html directly as root page
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
-
 // Serve map.html as /map
 app.get('/map', function(req, res) {
-    res.sendFile(path.join(__dirname, 'views/map.html'));
+  res.sendFile(path.join(__dirname, 'views/map.html'));
 });
-
 // Serve dispatcher.html as /dispatcher
 app.get('/dispatcher', function (req, res) {
   res.sendFile(path.join(__dirname, 'views/dispatcher.html'));
 });
 
-// Start the server
+// Store data in an object to keep the global namespace clean and 
+// prepare for multiple instances of data if necessary
+function Data() {
+  this.orders = {};
+  this.orderID = 0;
+}
+
+Data.prototype.getNext = function() {
+  return ++this.orderID;
+};
+
+/*
+  Adds an order to to the queue
+*/
+Data.prototype.addOrder = function (order) {
+  //Store the order in an "associative array" with orderId as key
+  this.orders[data.getNext()] = order;
+};
+
+Data.prototype.getAllOrders = function () {
+  return this.orders;
+};
+
+const data = new Data();
+
+io.on('connection', function (socket) {
+  // Send list of orders when a client connects
+  socket.emit('initialize', { orders: data.getAllOrders() });
+
+  // When a connected client emits an "addOrder" message
+  socket.on('addOrder', function (order) {
+    data.addOrder(order);
+    // send updated info to all connected clients, note the use of io instead of socket
+    io.emit('currentQueue', { orders: data.getAllOrders() });
+  });
+
+});
+
 const server = http.listen(app.get('port'), function () {
   console.log('Server listening on port ' + app.get('port'));
 });
-
 
 
 
